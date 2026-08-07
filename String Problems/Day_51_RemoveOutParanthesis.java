8d458af11b9d2af7b015ab48d91bf20d8c037b36
SRIKANTH K <2020tamilarasi115@gmail.com>
2026-08-07 21:51:38 +0530

    Day 51 is done

---
+++ String Problems/Day_51_RemoveOutParanthesis.java
@@ -0,0 +1,25 @@
+public class Day_51_RemoveOutParanthesis {
+    public static void main(String[] args) {
+        String s="((()))";
+        String result=removeOutParanthesis(s);
+        System.out.println(result);
+    }
+
+    public static String removeOutParanthesis(String s){
+        int count=0;
+        StringBuilder ans=new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==')'){
+                count--;
+            }
+            if(count!=0){
+                ans.append(s.charAt(i));
+            }
+            if (s.charAt(i)=='('){
+                count++;
+            }
+        }
+        return ans.toString();
+    }
+}
